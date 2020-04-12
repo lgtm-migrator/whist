30ef56700ca619fc3a25e79b36f2fa5e56fa48a7
Jørgen Fogh <jrgfogh@gmail.com>
2020-04-12 20:35:15 +0200

    Implemented PlayingRound.

---
+++ Whist.Rules.Tests/PlayingRoundTests.cs
@@ -0,0 +1,48 @@
+﻿using System.Linq;
+
+namespace Whist.Rules.Tests
+{
+    using NUnit.Framework;
+
+    public sealed class PlayingRoundTests
+    {
+        [Test]
+        public void Player0PlaysFirst()
+        {
+            var evaluator = new SansTrickEvaluator();
+            var round = new PlayingRound(evaluator);
+            Assert.That(round.PlayerToPlay, Is.EqualTo(0));
+        }
+
+        [Test]
+        [TestCase("H3", 1, null)]
+        [TestCase("H3\n" +
+            "H4", 2, null)]
+        [TestCase("H3\n" +
+                  "H4\n" +
+                  "H5", 3, null)]
+        [TestCase("H3\n" +
+                  "H4\n" +
+                  "H5\n" +
+                  "H2", 0, 2)]
+        [TestCase("H3\n" +
+                  "H4\n" +
+                  "H5\n" +
+                  "H7", 0, 3)]
+        [TestCase("H3\n" +
+                  "H4\n" +
+                  "H5\n" +
+                  "H7\n" +
+                  "C10", 1, null)]
+        public void Play(string cardNames, int playerToPlay, int? trickTaker)
+        {
+            var evaluator = new SansTrickEvaluator();
+            var round = new PlayingRound(evaluator);
+            int? lastTrickTaker = null;
+            foreach (var card in cardNames.Split('\n').Select(name => new Card(name)))
+                lastTrickTaker = round.Play(card);
+            Assert.That(lastTrickTaker, Is.EqualTo(trickTaker));
+            Assert.That(round.PlayerToPlay, Is.EqualTo(playerToPlay));
+        }
+    }
+}
---
+++ Whist.Rules/PlayingRound.cs
@@ -0,0 +1,34 @@
+﻿namespace Whist.Rules
+{
+    using System;
+    using System.Collections.Generic;
+
+    public sealed class PlayingRound
+    {
+        private readonly TrickEvaluator _evaluator;
+        private readonly List<Card> _cardsInTrick = new List<Card>();
+
+        public PlayingRound(TrickEvaluator evaluator)
+        {
+            _evaluator = evaluator;
+        }
+
+        public int? Play(Card card)
+        {
+            _cardsInTrick.Add(card);
+            PlayerToPlay = (PlayerToPlay + 1) % 4;
+            if (_cardsInTrick.Count == 4)
+                return WinnerTakesTrick();
+            return null;
+        }
+
+        private int WinnerTakesTrick()
+        {
+            var winner = _evaluator.EvaluateTrick(_cardsInTrick);
+            _cardsInTrick.Clear();
+            return winner;
+        }
+
+        public int PlayerToPlay { get; private set; }
+    }
+}
