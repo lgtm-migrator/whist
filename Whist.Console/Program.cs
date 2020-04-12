477b97aff1585064fe90f82bc06745194ac583fe
Jørgen Fogh <jrgfogh@gmail.com>
2020-04-12 22:07:59 +0200

    Added Whist.Console.

---
+++ Whist.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Whist.Console
+{
+    using System;
+
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the whist console!");
+        }
+    }
+}
