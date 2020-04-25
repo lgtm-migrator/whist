34a60401c2e3b65533ef7bad5181648de02d5d03
Jørgen Fogh <jrgfogh@gmail.com>
2020-04-25 19:11:47 +0200

    Web client: Added the cat and made cards clickable.

--- Whist.Server/ClientApp/src/components/Game.js
+++ Whist.Server/ClientApp/src/components/Game.js
@@ -4,6 +4,11 @@ import "./Game.css";
 
 export function Game(props) {
     return (<div className="game-background">
+        <div className="cat">
+            <div className="card C2"></div>
+            <div className="card S5"></div>
+            <div className="card HQ"></div>
+        </div>
         <Hand cards={["C1", "D3", "SQ"]} />
         </div>);
 }
--- Whist.Server/ClientApp/src/components/Hand.js
+++ Whist.Server/ClientApp/src/components/Hand.js
@@ -1,9 +1,16 @@
 ﻿import React from "react";
 
+function Card(props) {
+    function clickCard() {
+        // TODO(jorgen.fogh): Play the card!
+    }
+    return (<div className={"card " + props.name} onClick={clickCard}></div>);
+}
+
 export function Hand(props) {
     let cards = [];
     for (let i = 0; i < props.cards.length; i++)
-        cards.push(<div className={"card " + props.cards[i]}></div>);
+        cards.push(<Card name={props.cards[i]}></Card>);
     return (<div className="hand">
         { cards }
         </div>);
