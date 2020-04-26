6dbe812eaa7890f6bf1b0cb5195598adec0fc0ea
Jørgen Fogh <jrgfogh@gmail.com>
2020-04-26 23:06:00 +0200

    Web client: Wrote the ace picker.

--- Whist.Server/ClientApp/src/components/Game.js
+++ Whist.Server/ClientApp/src/components/Game.js
@@ -1,11 +1,11 @@
 ﻿import React from "react";
 import Hand from "./Hand.js";
 import Cat from "./Cat.js";
+import AcePicker from "./AcePicker.js";
 import "./Game.css";
 
 export function Game(props) {
     return (<div className="game-background">
-        <Cat cards={["C2", "S5", "HQ"]} />
-        <Hand cards={["C1", "D3", "SQ"]} />
+        <AcePicker onChoice={ace => alert(ace)} />
         </div>);
 }
